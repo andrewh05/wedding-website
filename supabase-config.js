fd290ca83b528fc8af92af55d0d55bd8f6c8a231
Andrew Haddad <andrewphaddad@gmail.com>
2026-06-07 16:13:23 +0300

    Add Supabase anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.WEDDING_SUPABASE = {
   url: "https://setzkulmkjjdjqtriwec.supabase.co",
-  anonKey: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNldHprdWxta2pqZGpxdHJpd2VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MTE3NTYsImV4cCI6MjA5NTk4Nzc1Nn0.xPx8cigX_IleNLcGNiGVSE1UiBBFaRYEANuZ5eLbiLI"
 };
